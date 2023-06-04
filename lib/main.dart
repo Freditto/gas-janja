4def6b82dc82f642c49ad626c8188ee0296d5779
Fred Herbert <fredittoherman@gmail.com>
2023-06-04 22:08:54 +0300

    commit

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:gas_janja/navigator.dart';
+import 'package:gas_janja/splash.dart';
 
 void main() {
   runApp(const MyApp());
@@ -18,7 +19,7 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
        
       ),
-      home: NavigatorWidget(),
+      home: SplashScreen(),
     );
   }
 }
--- lib/splash.dart
+++ lib/splash.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:gas_janja/navigator.dart';
 
 class SplashScreen extends StatefulWidget {
   @override
@@ -6,8 +7,54 @@ class SplashScreen extends StatefulWidget {
 }
 
 class _SplashScreenState extends State<SplashScreen> {
+  void _moveToHome() async {
+    await Future.delayed(
+      Duration(seconds: 2),
+    );
+    Navigator.pushAndRemoveUntil(
+        context,
+        MaterialPageRoute(builder: (context) => NavigatorWidget()),
+        (Route<dynamic> _) => false);
+  }
+
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    _moveToHome();
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return SafeArea(
+      child: Scaffold(
+        backgroundColor: Colors.white,
+        body: Container(
+          width: MediaQuery.of(context).size.width,
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: <Widget>[
+              Image.asset(
+                'assets/gaslogo.png',
+                width: 200,
+                height: 200,
+              ),
+              Text(
+                "GasJanja",
+                style: TextStyle(
+//                    color: Colors.black,
+                    fontWeight: FontWeight.bold,
+                    fontSize: 25.0),
+              ),
+              SizedBox(
+                height: 155,
+              )
+            ],
+          ),
+        ),
+      ),
+    );
   }
 }
+
